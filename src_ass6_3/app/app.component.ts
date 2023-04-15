2f8bb5cd6199df171436aa6494b2f82bed2fa067
vaishnavi0211 <128361543+vaishnavi0211@users.noreply.github.com>
2023-04-15 17:10:22 +0530

    Add files via upload

---
+++ src_ass6_3/app/app.component.ts
@@ -0,0 +1,12 @@
+import { style } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template:'<h1> Marvellous Infosystem </h1><input type="text">',
+  styles:['h1 { color: blue;}']
+})
+
+export class AppComponent {
+  title = 'Ass6';
+}
---
+++ src_ass6_4/app/newcomp/newcomp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newcomp',
+  templateUrl: './newcomp.component.html',
+  styleUrls: ['./newcomp.component.css']
+})
+export class NewcompComponent {
+  public Name='Marvellous';
+}
